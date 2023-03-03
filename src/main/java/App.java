0a7cfabc9441337493fa6e6237e0419deb45f2ad
Mike <hgelhem@gmail.com>
2023-03-03 10:36:31 +0300

    сократил строки в App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
     }
 }
